0169823b3c3cc73cf7a33089fe84fe1599efed2a
Andreas Eismann <a.eismann@senbax.de>
2024-05-23 11:54:08 +0200

    fix: Added TODO for removal.

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ import m from "mithril";
 import tagl from "tagl-mithril";
 import io from "socket.io/client-dist/socket.io";
 
-const { div, h1, p, pre, button ,a} = tagl(m);
+const { div, h1, p, pre, button, a } = tagl(m);
 const socket = io();
 
+// TODO: Remove this
 let code = "";
 m.request("/some").then((res) => {
   code = res;
@@ -14,7 +15,7 @@ const cookieValue = decodeURIComponent(
   document.cookie
     .split("; ")
     .find((row) => row.startsWith("session="))
-    ?.split("=")[1]
+    ?.split("=")[1],
 );
 
 socket.on("connect", () => {
@@ -33,6 +34,6 @@ m.mount(document.body, {
       h1("Tentacle testbed 2.0 " + cookieValue),
       p("This is a paragraph."),
       pre(m("code", code)),
-     a({href:"/logout"},"Logout"),
+      a({ href: "/logout" }, "Logout"),
     ]),
 });
