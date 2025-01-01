46f31c0f0ae7b9b317cf3650cac43b1e2997b192
Andreas Eismann <a.eismann@senbax.de>
2025-01-01 12:20:50 +0100

    feat: Adding state proxy to frontend

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,27 +1,33 @@
 import m from "mithril";
 import tagl from "tagl-mithril";
 import io from "socket.io/client-dist/socket.io";
+import tentacle from "../tentacle.js";
 
 const { div, h1, p, pre, button, a } = tagl(m);
 const socket = io();
 
-// TODO: Remove this
-let code = "";
-m.request("/some").then((res) => {
-  code = res;
-});
+const _state = {};
+
+const sendPatch = (patch) => {
+  socket.emit("patch", patch);
+};
+const state = tentacle(_state, sendPatch);
 
 const cookieValue = decodeURIComponent(
   document.cookie
     .split("; ")
     .find((row) => row.startsWith("session="))
-    ?.split("=")[1],
+    ?.split("=")[1]
 );
 
 socket.on("connect", () => {
   const userID =
     localStorage.getItem("id") || localStorage.setItem("id", socket.id);
   socket.emit("i am", { id: userID });
+  socket.on("state", (newState) => {
+    Object.assign(_state, newState);
+    m.redraw();
+  });
 });
 
 socket.on("disconnect", () => {
@@ -33,7 +39,6 @@ m.mount(document.body, {
     div([
       h1("Tentacle testbed 2.0 " + cookieValue),
       p("This is a paragraph."),
-      pre(m("code", code)),
       a({ href: "/logout" }, "Logout"),
     ]),
 });
